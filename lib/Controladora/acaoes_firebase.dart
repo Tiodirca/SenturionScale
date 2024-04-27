23c40f054b2194c61b014b0a8648ebd63b1298b6
jhona <jhonatansbestevao@gmail.com>
2024-04-27 16:50:47 -0300

    finalizacoes de configuracoes

---
+++ lib/Controladora/acaoes_firebase.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../Modelos/tabelas_modelo.dart';
+import '../Uteis/constantes.dart';
+import '../Uteis/metodos_auxiliares.dart';
+
+class AcoesFireBase {
+  static criarTabelas(String nomeTabela) {
+    CollectionReference colecao = FirebaseFirestore.instance
+        .collection(Constantes.fireBaseTabelasColecao);
+    colecao.add({
+      Constantes.fireBaseValorTabelas:
+          MetodosAuxiliares.removerEspacoNomeTabelas(nomeTabela)
+    });
+  }
+
+  static Future consultarTabelas() async {
+    List<TabelaModelo> tabelasBancoDados = [];
+    var db = FirebaseFirestore.instance;
+    await db.collection(Constantes.fireBaseTabelasColecao).get().then((event) {
+      for (var doc in event.docs) {
+        var nomeTabela = doc
+            .data()
+            .values
+            .toString()
+            .replaceAll("(", "")
+            .replaceAll(")", "");
+        tabelasBancoDados
+            .add(TabelaModelo(nomeTabela: nomeTabela, idTabela: doc.id));
+      }
+    });
+    return tabelasBancoDados;
+  }
+}
