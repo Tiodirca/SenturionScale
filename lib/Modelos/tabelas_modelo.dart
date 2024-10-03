1883483f407819f9712914967deb7d00c4cdb2e3
jhona <jhonatansbestevao@gmail.com>
2024-10-03 18:53:06 -0300

    adicao de novas opcoes de opcoes de data

--- lib/Modelos/tabelas_modelo.dart
+++ lib/Modelos/tabelas_modelo.dart
@@ -1,24 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class TabelaModelo {
   TabelaModelo({required this.nomeTabela, required this.idTabela});
 
   String nomeTabela;
   String idTabela;
 
-// factory TabelaModelo.fromFirestore(
-//     DocumentSnapshot<Map<dynamic, dynamic>> snapshot,
-//     SnapshotOptions? options,
-//     ) {
-//   final data = snapshot.data();
-//   return TabelaModelo(
-//     nomeTabela: data?['nome_tabela'],
-//   );
-// }
-//
-// Map<String, dynamic> toFirestore() {
-//   return {
-//     "nome_tabela": nomeTabela,
-//   };
-// }
 }
